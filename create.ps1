5491565484bd869e571d03b1c6697d8a2a659053
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-08-06 13:30:12 -0400

    Auto commit

--- create.ps1
+++ create.ps1
@@ -1,4 +1,5 @@
 # Run as Administrator
+Remove-Item -Path 'C:\Program Files\Toxik\Jokes'
 New-Item -Path 'C:\Program Files\Toxik' -ItemType Directory
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/maverick-dev-55/joke-app/main/dist/Jokes.zip' -OutFile 'C:\Program Files\Toxik\Jokes.zip'
 Expand-Archive -LiteralPath 'C:\Program Files\Toxik\Jokes.zip' -DestinationPath 'C:\Program Files\Toxik\'
