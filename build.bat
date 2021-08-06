33599e05306e676d33c6f0271057ca9835a916e4
MaverickQuill55 (VS Code) <VScode@toxik.cf>
2021-08-06 10:18:21 -0400

    beta fixed

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
 python -m PyInstaller -n Jokes --add-binary icon.png;icon.png -i icon.ico -D --noconfirm main.py
+powershell compress-archive -path "/dist/Jokes" "/dist/Jokes.zip" -compressionlevel optimal
